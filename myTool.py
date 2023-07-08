3d2a85c140157bea90db1dc7e4f720b974248908
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-07-08 11:52:34 +0700

    added view_random_image

--- myTool.py
+++ myTool.py
@@ -4,6 +4,9 @@ import numpy as np
 from typing import List
 import tensorflow as tf
 import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
 
 
 def calculate_results(y_true: np.ndarray, y_pred: np.ndarray) -> dict:
@@ -113,3 +116,22 @@ def evaluate_preds(y_true, y_pred):
             "rmse": rmse.numpy(),
             "mape": mape.numpy(),
             "mase": mase.numpy()}
+
+
+def view_random_image(target_dir: str, target_class: str):
+    """
+    Plots a random image from target_dir and target_class
+    :param target_dir:
+    :param target_class:
+    :return:
+    >> view_random_image(view_random_image(target_dir="pizza_steak/train/",
+                        target_class="steak"))
+    """
+
+    random_img = random.sample(os.listdir(target_dir + target_class), 1)
+    img = mpimg.imread(target_dir + target_class + "/" + random_img[0])
+    plt.imshow(img)
+    plt.title(target_class)
+    plt.axis("off")
+    print(f"Image shape: {img.shape}")
+    return img
